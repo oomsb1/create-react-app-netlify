612896c02ad56b750f0a499cc03c36dc727d5df7
Bram Ooms <58232023+oomsb1@users.noreply.github.com>
2022-05-31 22:43:39 +0200

    trying something

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../style.css";
 import HomePagina from "./homepagina";
 import CV from "./cv";
